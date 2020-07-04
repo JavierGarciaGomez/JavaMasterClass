0075012929a04afe36e020a17e71bbd4ee864e9d
JGG <javieron.garcia@gmail.com>
2020-07-04 21:00:52 +0200

    Section 18, END

--- S14_17/src/S18DebuggingAndTesting/l321challenges1_2/Main.java
+++ S14_17/src/S18DebuggingAndTesting/l321challenges1_2/Main.java
@@ -1,4 +1,8 @@
 package S18DebuggingAndTesting.l321challenges1_2;
 
 public class Main {
+    public static void main(String[] args) {
+        Utilities utilities = new Utilities();
+        utilities.removePairs(null);
+    }
 }
--- S14_17/src/S18DebuggingAndTesting/l321challenges1_2/UtilitiesTestTryChallenge10.java
+++ S14_17/src/S18DebuggingAndTesting/l321challenges1_2/UtilitiesTestTryChallenge10.java
@@ -10,13 +10,12 @@ import java.util.Collection;
 import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
-public class UtilitiesTestTry {
+public class UtilitiesTestTryChallenge10 {
     private Utilities utilities;
-    private String msgNotImplemented = "******This test has not been implemented******";
     private String original;
     private String expected;
 
-    public UtilitiesTestTry(String original, String expected) {
+    public UtilitiesTestTryChallenge10(String original, String expected) {
         this.original = original;
         this.expected = expected;
     }
@@ -31,7 +30,9 @@ public class UtilitiesTestTry {
     @Parameterized.Parameters
     public static Collection<Object[]> testConditions() {
         return Arrays.asList(new Object[][] {
-                {"ABCCABDEEF", true, "ABCABDEF"},
+                {"ABCDEFF", "ABCDEF"},
+                {"AB88EFFG", "AB8EFG"},
+                {"A", "A"}
         });
     }
 
@@ -40,52 +41,4 @@ public class UtilitiesTestTry {
         assertEquals(expected, utilities.removePairs(original));
     }
 
-    // challenge 4
-    @org.junit.Test
-    public void everyNthChar() {
-        assertArrayEquals(new char[]{'e','l'}, utilities.everyNthChar(new char[]{'h','e','l','l','o'}, 2));
-    }
-
-    // challenge 5
-    @org.junit.Test
-    public void everyNthCharChall5() {
-        char [] sourceArray = new char[]{'a','b','c','d'};
-        assertArrayEquals(sourceArray, utilities.everyNthChar(sourceArray, 10)); // more than the length
-    }
-
-    // challenge 2
-    @org.junit.Test
-    public void removePairs() {
-        String testString = utilities.removePairs("AABCDDEFF");
-        assertEquals("ABCDEF", testString);
-        assertEquals("ABCABDEF", utilities.removePairs("ABCCABDEEF"));
-    }
-
-    //challenge 3
-    @org.junit.Test
-    public void removePairsChallenge3() {
-        assertEquals("A", utilities.removePairs("A")); // less than 2 test
-        assertNull(null, utilities.removePairs(null)); // Null
-        //assertNull("Did not get null retrurned", utilities.removePairs(null));
-    }
-
-    //challenge 7
-    @org.junit.Test
-    public void converter() {
-        assertEquals(300, utilities.converter(10,5));
-    }
-
-    //challenge 8
-    @org.junit.Test(expected = ArithmeticException.class)
-    public void converterChall8() throws Exception{
-        utilities.converter(10,0);
-    }
-
-
-    // challenge 6
-    @org.junit.Test
-    public void nullIfOddLength() {
-        assertEquals("abcd", utilities.nullIfOddLength("abcd"));
-        assertNull(utilities.nullIfOddLength("abcde"));
-    }
 }

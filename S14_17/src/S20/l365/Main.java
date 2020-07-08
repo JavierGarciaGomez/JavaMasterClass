5cc8f9cbecdc67f7f1064993aca7ae964bc4e1be
JGG <javieron.garcia@gmail.com>
2020-07-08 18:32:19 +0200

    Section 20, END (365-367)

--- S14_17/src/S20/l365/Main.java
+++ S14_17/src/S20/l365/Main.java
@@ -1,40 +1,42 @@
 package S20.l365;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-            System.out.println("Scheme = " + uri.getScheme());
-            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-            System.out.println("Authority = " + uri.getAuthority());
-            System.out.println("User info = " + uri.getUserInfo());
-            System.out.println("Host = " + uri.getHost());
-            System.out.println("Port = " + uri.getPort());
-            System.out.println("Path = " + uri.getPath());
-            System.out.println("Query = " + uri.getQuery());
-            System.out.println("Fragment = " + uri.getFragment());
+            URL url = new URL("http://example.org");
 
-            URI baseUri = new URI("http://username:password@myserver.com:5000");
-            uri = new URI("/catalogue/phones?os=android#samsung");
-            URI resolvedUri = baseUri.resolve(uri);
+            System.out.println("FIRST WAY");
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line = "";
+            while(line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
 
-            URL url = resolvedUri.toURL();
-            System.out.println("URL = " + url);
-            uri = new URI("hello");
+            System.out.println("SECOND WAY");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
 
+            inputStream = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
 
+            line = "";
+            while(line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+            inputStream.close();
 
-        } catch(URISyntaxException e) {
-            System.out.println("URI Bad Syntax: " + e.getMessage());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+        } catch(MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
+        } catch(IOException e) {
+            System.out.println("IOException: " + e.getMessage());
         }
-
-
     }
 }
